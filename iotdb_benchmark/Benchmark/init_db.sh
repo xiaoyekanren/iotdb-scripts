e205d519f15f2c7cbf479b12997300be29981a8a
xiaoyekanren <zhengming.zhang@timecho.com>
2024-04-10 19:28:42 +0800

    change scripts to suppoort iotdb1.x

--- iotdb_benchmark/Benchmark/init_db.sh
+++ iotdb_benchmark/Benchmark/init_db.sh
@@ -1,36 +1,208 @@
 #!/bin/bash
 
-get_para() {
+singal() {
     case "$1" in
     iotdb)
-        # 必须填写IOTDB路径, 必须export java，要不大概率找不到
-        # if data路径有变化，需手动修改
-        IOTDB_HOME=/home/ubuntu/release_0.13_test/iotdb-0.13.1-snopshot
-        JAVA_HOME=/usr/local/jdk1.8.0_321
-        export PATH=$JAVA_HOME/bin:$PATH
-        # auto generate
-        IOTDB_BIN=$IOTDB_HOME/sbin
-        IOTDB_DATA=$IOTDB_HOME/data
+        iotdb_operate "get_para"
+        echo ${OPERATE}
+        iotdb_operate ${OPERATE}
         ;;
     influxdb)
+        echo influxdb
+        ;;
+    kairosdb)
+        echo kairosdb
+        ;;
+    taos)
+        echo taos
+        ;;
+    timescaledb)
+        echo timescaledb
+        ;;
+    esac
+}
+
+iotdb_operate() {
+    case "$1" in
+    get_para)
+        echo "get ${DB} paras."
+        # 必须填写IOTDB路径, 必须export java，要不大概率找不到
+        # if data路径有变化，需手动修改
+        IOTDB_HOME=/root/data/20240401-timechodb-1321-rc1-da56ef1b8c
+        JAVA_HOME=/usr/local/jdk-11.0.17
+        # auto generate
+        export PATH=$JAVA_HOME/bin:$PATH
+        IOTDB_SBIN=$IOTDB_HOME/sbin
+        IOTDB_DATA=$IOTDB_HOME/data
+        IOTDB_LOG=$IOTDB_HOME/logs
+        ;;
+    clear)
+        echo "clear iotdb."
+        rm -rf $IOTDB_DATA
+        ;;
+    start)
+        echo "start iotdb."
+        $IOTDB_SBIN/start-standalone.sh
+        echo $!
+        ;;
+    stop)
+        echo "stop iotdb."
+        $IOTDB_SBIN/stop-standalone.sh -f || true
+        echo $!
+        ;;
+    backup_log)
+        echo "backup log."
+        # 没找到备份参数
+        if [ "${backup_folder}" == "/" ]; then
+            echo "没有找到备份目录参数."
+            if [ -d "${IOTDB_LOG}" ]; then
+                echo "发现了logs目录，清理."
+                rm -rf ${IOTDB_LOG}
+            fi
+        else
+            if [ -d "${IOTDB_LOG}" ]; then
+                mkdir -p "${IOTDB_HOME}/backup/${backup_folder}"
+                # 复制IOTDB_LOG到新创建的目录
+                mv "${IOTDB_LOG}" "${IOTDB_HOME}/backup/${backup_folder}/"
+            else
+                echo "Directory ${IOTDB_LOG} does not exist.异常退出"
+                exit 1
+            fi
+        fi
+        ;;
+    show_cluster)
+        echo "show iotdb cluster"
+        ${IOTDB_SBIN}/start-cli.sh -e show cluster
+        ;;
+    get_pid)
+        echo $(${JAVA_HOME}/bin/jps -l | grep 'org.apache.iotdb.db.service.IoTDB' | awk '{print $1}')
+        ;;
+    esac
+}
+
+influxdb_operate() {
+    case "$1" in
+    get_para)
+        echo "get ${DB} paras."
         # influxdb,必须指定以下4个参数
         INFLUXDB_HOME=/home/zzm/data/influxdb-1.5.5-1
         INFLUXDB_BIN=$INFLUXDB_HOME/usr/bin
         INFLUXDB_DATA=$INFLUXDB_HOME/data
         INFLUXDB_CONF_FILE=$INFLUXDB_HOME/etc/influxdb/influxdb.conf
         ;;
-    kairosdb)
+    clear)
+        echo "clear ${DB}."
+        rm -rf $INFLUXDB_DATA
+        ;;
+    start)
+        echo "start ${DB}."
+        cd $INFLUXDB_HOME
+        nohup $INFLUXDB_BIN/influxd -config $INFLUXDB_CONF_FILE >nohup.out 2>&1 &
+        echo $!
+        sleep 10
+        ;;
+    stop)
+        echo "stop ${DB}."
+        cd $INFLUXDB_HOME
+        nohup $INFLUXDB_BIN/influxd -config $INFLUXDB_CONF_FILE >nohup.out 2>&1 &
+        echo $!
+        sleep 10
+        ;;
+    backup_log)
+        echo "backup ${DB} log."
+        ;;
+    get_pid)
+        echo $(ps -ef | grep '[i]nfluxd -config' | awk {'print $2'})
+
+        ;;
+    esac
+}
+
+kairosdb_operate() {
+    case "$1" in
+    get_para)
+        echo "get ${DB} paras."
         # 该脚本远程使用时，因环境变量设置位置不同可能会找不到java，需要手动配置JAVA_HOME
         # bin/kairosdb.sh,bin/kairosdb-service.sh 需指定export JAVA_HOME=xxxx
         # cassandra/bin/cassandra 需指定export JAVA_HOME=xxxx
         CASSANDRA_HOME=/home/zzm/data/kairosdb/apache-cassandra-3.11.2
         KAIROSDB_HOME=/home/zzm/data/kairosdb/kairosdb
         ;;
-    taos)
+    clear)
+        echo "clear ${DB}."
+        # clear-cassandra
+        rm -rf $CASSANDRA_HOME/data
+        rm -rf $CASSANDRA_HOME/logs
+        # clear-kairosdb
+        rm -rf $KAIROSDB_HOME/queue
+        ;;
+    start)
+        echo "start ${DB}."
+        nohup $CASSANDRA_HOME/bin/cassandra -f >/dev/null 2>&1 &
+        echo $!
+        sleep 40
+        # start-kairosdb...
+        $KAIROSDB_HOME/bin/kairosdb.sh start
+        echo $!
+        sleep 20
+        ;;
+    stop)
+        echo "stop ${DB}."
+        nohup $CASSANDRA_HOME/bin/cassandra -f >/dev/null 2>&1 &
+        echo $!
+        sleep 40
+        # start-kairosdb...
+        $KAIROSDB_HOME/bin/kairosdb.sh start
+        echo $!
+        sleep 20
+        ;;
+    backup_log)
+        echo "backup ${DB} log."
+        ;;
+    get_pid)
+        cassandra_pid=$(ps -ef | grep '[o]rg.apache.cassandra.service.CassandraDaemon' | awk '{print $2}')
+        kairos_pid=$(ps -ef | grep "[o]rg.kairosdb.core.Main -c start -p" | awk '{print $2}')
+        echo $cassandra_pid $kairos_pid
+        ;;
+    esac
+}
+
+taos_operate() {
+    case "$1" in
+    get_para)
+        echo "get ${DB} paras."
         # 修改taos的数据目录即可
         TAOS_DATA=/home/zzm/data/taos_data
         ;;
-    timescaledb)
+    clear)
+        echo "clear ${DB}."
+        sudo rm -rf $TAOS_DATA/*
+        ;;
+    start)
+        echo "start ${DB}."
+        sudo systemctl start taosd
+        sleep 10
+        echo $!
+        ;;
+    stop)
+        echo "stop ${DB}."
+        sudo systemctl start taosd
+        sleep 10
+        echo $!
+        ;;
+    backup_log)
+        echo "backup ${DB} log."
+        ;;
+    get_pid)
+        echo "no necessary."
+        ;;
+    esac
+}
+
+timescaledb_operate() {
+    case "$1" in
+    get_para)
+        echo "get ${DB} paras."
         # 本脚本用于二进制包安装的postgresql和编译安装的timescaledb，且timescaledb已经加载到了pg下
         # PG_MAIN存放pg的数据文件夹和可执行程序
         PG_MAIN=/home/zzm/data/timescaledb
@@ -40,83 +212,12 @@ get_para() {
         PG_BIN=$PG_HOME/bin
         LOG=$PG_HOME/log/$(date +"%Y-%m-%d-%H-%M-%S").log
         ;;
-    esac
-}
-
-get_pid() {
-    case "$1" in
-    iotdb)
-        echo $(${JAVA_HOME}/bin/jps -l | grep 'org.apache.iotdb.db.service.IoTDB' | awk '{print $1}')
-        ;;
-    influxdb)
-        echo $(ps -ef | grep '[i]nfluxd -config' | awk {'print $2'})
-        ;;
-    kairosdb)
-        cassandra_pid=$(ps -ef | grep '[o]rg.apache.cassandra.service.CassandraDaemon' | awk '{print $2}')
-        kairos_pid=$(ps -ef | grep "[o]rg.kairosdb.core.Main -c start -p" | awk '{print $2}')
-        echo $cassandra_pid $kairos_pid
-        ;;
-    taos)
-        echo "no necessary."
-        ;;
-    timescaledb)
-        echo $(ps -ef | grep '[p]gsql/bin/postgres -D' | awk '{print $2}')
-        ;;
-    esac
-}
-
-clear_db() {
-    case "$1" in
-    iotdb)
-        rm -rf $IOTDB_DATA
-        ;;
-    influxdb)
-        rm -rf $INFLUXDB_DATA
-        ;;
-    kairosdb)
-        # clear-cassandra
-        rm -rf $CASSANDRA_HOME/data
-        rm -rf $CASSANDRA_HOME/logs
-        # clear-kairosdb
-        rm -rf $KAIROSDB_HOME/queue
-        ;;
-    taos)
-        sudo rm -rf $TAOS_DATA/*
-        ;;
-    timescaledb)
+    clear)
+        echo "clear ${DB}."
         rm -rf $PG_DATA/*
         ;;
-    esac
-}
-
-start_db() {
-    case "$1" in
-    iotdb)
-        nohup $IOTDB_BIN/start-server.sh >/dev/null 2>&1 &
-        echo $!
-        sleep 10
-        ;;
-    influxdb)
-        cd $INFLUXDB_HOME
-        nohup $INFLUXDB_BIN/influxd -config $INFLUXDB_CONF_FILE >nohup.out 2>&1 &
-        echo $!
-        sleep 10
-        ;;
-    kairosdb)
-        nohup $CASSANDRA_HOME/bin/cassandra -f >/dev/null 2>&1 &
-        echo $!
-        sleep 40
-        # start-kairosdb...
-        $KAIROSDB_HOME/bin/kairosdb.sh start
-        echo $!
-        sleep 20
-        ;;
-    taos)
-        sudo systemctl start taosd
-        sleep 10
-        echo $!
-        ;;
-    timescaledb)
+    start)
+        echo "start ${DB}."
         $PG_BIN/initdb -d $PG_DATA
         sed -i "s:^max_connections.*:max_connections = 1000:g" $PG_DATA/postgresql.conf
         sed -i "s/^#listen_addresses =.*/listen_addresses = '*'/g" $PG_DATA/postgresql.conf
@@ -157,12 +258,20 @@ start_db() {
         $PG_BIN/psql -U postgres -c "CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;"
         echo 'finish'
         ;;
+    stop)
+        echo "stop ${DB}."
+        ;;
+    backup_log)
+        echo "backup ${DB} log."
+        ;;
+    get_pid)
+        echo $(ps -ef | grep '[p]gsql/bin/postgres -D' | awk '{print $2}')
+        ;;
     esac
 }
 
-main() {
-    echo 123
-}
-
-cur_switch=$1
-main
+echo "para1 = ${1}, para2 = ${2}, para3 = ${3}"
+DB=$1
+OPERATE=$2
+backup_folder=$3
+singal $1
--- iotdb_benchmark/Benchmark/test_moreloop.sh
+++ iotdb_benchmark/Benchmark/test_moreloop.sh
@@ -8,49 +8,53 @@ set -e
 
 # -----!!!需要确定的参数!!!-----
 # benchmark路径
-BENCHMARK_HOME="/home/ubuntu/release_0.13_test/benchmark"
+BENCHMARK_HOME="/root/data/bm-1.3"
 # 仅作为log记录，和初始化使用，必须小写
 # support db: iotdb, influxdb, kairosdb, taos, timescaledb
 DB="iotdb"
 # DB信息
-DB_HOST="ubuntu@172.20.31.22"
-INIT_SCRIPT_PATH="/home/ubuntu/release_0.13_test/iotdb-0.13.1-snopshot/init_db.sh"
+HOST_USER="root"
+INIT_SCRIPT_PATH="/root/data/20240401-timechodb-1321-rc1-da56ef1b8c/init_db.sh"
+HOSTS="11.101.17.224,11.101.17.225,11.101.17.226"
 # 其他信息
 SLEEP_TIME=5
+# 远端IOTDB的目录
+IOTDB_HOME=/root/data/20240401-timechodb-1321-rc1-da56ef1b8c
 
 # 检查远程信息
 check_db_host() {
     cur_time=$(date "+%s")
-    tmp_file="/tmp/${cur_time}_abc.txt"
+    tmp_file="/tmp/${cur_time}_${DB}.txt"
     error_message="Cannot remote to database server without password, please check."
-    nohup ssh $DB_HOST whoami >$tmp_file 2>&1 &
-    nohup_pid=$!
-    sleep 3
-    ps_result_pid=$(ps -aux | grep $nohup_pid | grep -v 'grep --color=auto' | grep -v '[g]rep' | awk '{print $2}')
-    if [ ! "${ps_result_pid}1" == "1" ]; then
-        kill -9 $ps_result_pid
-        echo -e "\nCheck failed. process did not exit.\n"$error_message
-        stty sane
-        exit 1
-    else
-        if [ -f "/tmp/${cur_time}_abc.txt" ]; then
-            user=$(echo $DB_HOST | cut -d '@' -f1)
-            result=$(cat $tmp_file | grep -v "Warning: Permanently added")
-            if [ "$user" == "$result" ]; then
-                echo -e "remote to db server successed."
+    for ip in "${IP_LIST[@]}"; do
+        nohup ssh ${HOST_USER}@${ip} whoami >${tmp_file} 2>&1 &
+        nohup_pid=$!
+        sleep 1
+        ps_result_pid=$(ps -aux | grep $nohup_pid | grep -v 'grep --color=auto' | grep -v '[g]rep' | awk '{print $2}')
+        if [ ! "${ps_result_pid}1" == "1" ]; then
+            kill -9 $ps_result_pid
+            echo -e "\nCheck failed. process did not exit.\n"$error_message
+            stty sane
+            exit 1
+        else
+            if [ -f "${tmp_file}" ]; then
+                result=$(cat $tmp_file | grep -v "Warning: Permanently added")
+                if [ "${HOST_USER}" == "$result" ]; then
+                    echo -e "remote to db server successed."
+                else
+                    echo -e "\nCheck failed. get results error.\n"$error_message
+                    rm -rf $tmp_file
+                    exit 1
+                fi
             else
-                echo -e "\nCheck failed. get results error.\n"$error_message
+                echo -e "\nCheck failed. File no exists.\n"$error_message
                 rm -rf $tmp_file
                 exit 1
             fi
-        else
-            echo -e "\nCheck failed. File no exists.\n"$error_message
-            rm -rf $tmp_file
-            exit 1
         fi
-    fi
-}
+    done
 
+}
 # 批量替换配置文件
 alter_static_paras() {
     for alone in ${!static_paras[@]}; do
@@ -67,11 +71,38 @@ init_config() {
 # 初始化数据库
 # 将init_db.sh脚本丢到数据库服务器下
 init_db() {
-    ssh ${DB_HOST} "/bin/bash ${INIT_SCRIPT_PATH} ${DB}"
+    # stop
+    for ip in "${IP_LIST[@]}"; do
+        echo "ssh ${ip}."
+        ssh ${HOST_USER}@${ip} "/bin/bash -c \"${INIT_SCRIPT_PATH} ${DB} stop \""
+    done
+    sleep 3
+
+    # clear
+    for ip in "${IP_LIST[@]}"; do
+        echo "ssh ${ip}."
+        ssh ${HOST_USER}@${ip} "/bin/bash -c \"${INIT_SCRIPT_PATH} ${DB} backup_log ${1} \""
+        ssh ${HOST_USER}@${ip} "/bin/bash -c \"${INIT_SCRIPT_PATH} ${DB} clear \""
+    done
+
+    # start
+    for ip in "${IP_LIST[@]}"; do
+        echo "ssh ${ip}."
+        ssh ${HOST_USER}@${ip} "/bin/bash -c \"${INIT_SCRIPT_PATH} ${DB} start \""
+    done
+    sleep 3
+
+    # show cluster
+    for ip in "${IP_LIST[@]}"; do
+        echo "ssh ${ip}."
+        ssh ${HOST_USER}@${ip} "/bin/bash -c \"${INIT_SCRIPT_PATH} ${DB} show_cluster \""
+    done
+    sleep 3
 }
 # 主程序
 main() {
-    LOG_DIRECTORY=$WORK_DIRECTORY/$DB-$DYNAMIC_PARA-$(date +%Y%m%d_%H%M)
+    LOG_FOLDER=${DB}-${DYNAMIC_PARA}-$(date +%Y%m%d_%H%M)
+    LOG_DIRECTORY=${WORK_DIRECTORY}/${LOG_FOLDER}
     echo -e "mkdir record folder..." # 创建log文件夹
     mkdir -p $LOG_DIRECTORY
     for para in ${DYNAMIC_PARA_VALUES[@]}; do
@@ -81,11 +112,11 @@ main() {
         echo -e "2. modify dynamic paramater...\nchange ${DYNAMIC_PARA} to $para"
         sed -i -e "s/^${DYNAMIC_PARA}=.*/${DYNAMIC_PARA}=$para/g" $BENCHMARK_CONF_FILE
         echo -e "3. start benchmark...\nresults redirect to $LOG_DIRECTORY/${para}.out" # 启动程序
-        $BENCHMARK_EXEC_FILE >$LOG_DIRECTORY/${para}.out
+        $BENCHMARK_EXEC_FILE >$LOG_DIRECTORY/${para}.out 2>&1 
         echo -e "4. init config" # 恢复原始配置
         init_config
         echo -e "5. init ${DB}..."
-        init_db
+        init_db "${LOG_FOLDER}-${para}"
         echo -e "6. 执行结束，等待${SLEEP_TIME}秒"
         echo "----------end, waiting...----------"
         sleep $SLEEP_TIME
@@ -98,7 +129,7 @@ BENCHMARK_CONF_FILE="${BENCHMARK_CONF}/config.properties"
 BENCHMARK_CONF_FILE_BAK="${BENCHMARK_CONF_FILE}_$(date +%Y%m%d)_back"
 BENCHMARK_EXEC_FILE="${BENCHMARK_HOME}/benchmark.sh"
 WORK_DIRECTORY="${BENCHMARK_HOME}/work_log"
-
+IFS=',' read -ra IP_LIST <<<"$HOSTS"
 # 数据库信息：
 # DB, DB_SWITCH, PORT, USERNAME, PASSWORD, DB_NAME
 # IoTDB, IoTDB-01x-SESSION_BY_TABLET, 6667, root, root, test
@@ -108,11 +139,11 @@ WORK_DIRECTORY="${BENCHMARK_HOME}/work_log"
 # Timescaledb, TimescaleDB, 5432, postgres, 123456, postgres
 
 # 准备工作
-# 检查数据库服务器连接状态
+# 检查服务器连接状态
 check_db_host
 # 初始化数据库
 echo "init ${DB}..."
-init_db
+init_db "/"
 # 备份配置文件
 echo "backup config file..."
 if [ ! -f "${BENCHMARK_CONF_FILE_BAK}" ]; then
@@ -121,34 +152,52 @@ if [ ! -f "${BENCHMARK_CONF_FILE_BAK}" ]; then
 fi
 # 若需要长时间多次循环的测试，从这里往后复制到"round 1 end"
 # -----round 1-----
-DYNAMIC_PARA="BATCH_SIZE_PER_WRITE"
-DYNAMIC_PARA_VALUES=(1 10 100)
+DYNAMIC_PARA="ENABLE_RECORDS_AUTO_CONVERT_TABLET"
+# 括号里空格分隔，多个参数
+DYNAMIC_PARA_VALUES=(true false)
 # 声明用于参数修改的字典, 必须使用bash执行
 declare -A static_paras
 static_paras=(
     # 数据库连接信息
-    [DB_SWITCH]="IoTDB-013-SESSION_BY_TABLET"
-    [HOST]="172.20.31.22"
-    [PORT]="6667"
-    [USERNAME]='root'
-    [PASSWORD]='root'
-    [DB_NAME]='test'
+    [DB_SWITCH]="IoTDB-130-SESSION_BY_RECORDS"
+    [HOST]="${HOSTS}"
+    [PORT]="6667,6667,6667"
     # 时长
-    [TEST_MAX_TIME]="3600000"
-    [LOOP]="999999"
+    [TEST_MAX_TIME]="60000"
+    [LOOP]="99999999"
     # 数据量
-    [CLIENT_NUMBER]="1"
-    [GROUP_NUMBER]="1"
-    [DEVICE_NUMBER]="10"
-    [SENSOR_NUMBER]="10"
-    # 数据类型
-    [INSERT_DATATYPE_PROPORTION]="0:0:0:0:1:0"
-    [ENCODING_DOUBLE]="GORILLA"
+    [DEVICE_NUMBER]="100"
+    [SENSOR_NUMBER]="100"
     # 其他
-    [OPERATION_PROPORTION]="1:0:0:0:0:0:0:0:0:0:0"
+    [CLIENT_NUMBER]="10"
     [IS_DELETE_DATA]="true"
-    [BENCHMARK_WORK_MODE]="testWithDefaultPath"
-    [POINT_STEP]="10"
+    [POINT_STEP]="1000"
+    [VECTOR]="true"
+    [IS_DELETE_DATA]="false"
 )
 main
 # -----round 1 end-----
+# -----round 2-----
+DYNAMIC_PARA="ENABLE_RECORDS_AUTO_CONVERT_TABLET"
+# 括号里空格分隔，多个参数
+DYNAMIC_PARA_VALUES=(true false)
+# 声明用于参数修改的字典, 必须使用bash执行
+declare -A static_paras
+static_paras=(
+    # 数据库连接信息
+    [DB_SWITCH]="IoTDB-130-SESSION_BY_RECORDS"
+    [HOST]="${HOSTS}"
+    # 时长
+    [TEST_MAX_TIME]="60000"
+    [LOOP]="99999999"
+    # 数据量
+    [DEVICE_NUMBER]="6000"
+    [SENSOR_NUMBER]="200"
+    # 其他
+    [CLIENT_NUMBER]="10"
+    [IS_DELETE_DATA]="true"
+    [POINT_STEP]="1000"
+    [VECTOR]="false"
+)
+main
+# -----round 2 end-----
