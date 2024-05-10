37a34ca9196a28dbdbb72c3bcc513db1abc49882
xiaoyekanren <zhengming.zhang@timecho.com>
2024-05-10 09:20:58 +0800

    update get_result.sh

--- iotdb_benchmark/Benchmark/get_result.sh
+++ iotdb_benchmark/Benchmark/get_result.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
 
-# 当前拼出来的get语句
+# 工作目录
+word_dir="/root/format_scripts"
 
-# ## Create schema cost
-# cat 1.out |grep "\-Result Matrix\-" -B 2 | head -n 1 | cut -d ' ' -f 4
+# 结果文件夹
+results_folder="/root/data/rn1321-rc2/test_records_to_tablet/benchmark-out"
 
-# ## Test elapsed time
-# cat 1.out |grep "\-Result Matrix\-" -B 1 | head -n 1 | cut -d ' ' -f 8
+# 删除目录
+out_path="/root/format_scripts/$(date +%s)/iotdb"
 
-## 
+# 格式化的最终结果
+format_file="/root/format_scripts/format.txt"
+
+# 创建临时文件的文件夹
+tmp_folder="${word_dir}/$(date +%s)_tmp"
+echo "mkdir -p ${tmp_folder}"
+mkdir -p ${tmp_folder}
+
+# 拷贝 结果文件夹 的out文件到 临时文件夹
+echo "find ${results_folder} -name \"*.out\" | xargs -I {} cp {} ${tmp_folder}"
+find ${results_folder} -name "*.out" | xargs -I {} cp {} ${tmp_folder}
 
 
-abc="1:0:0:0:0:0:0:0:0:0:0"
-b=1
-while (( $b <= `echo $abc | awk '{n=split($0, array, ":")} END{print n}'` )); do
-def=$(echo $abc | cut -d ':' -f $b)
-let 'b+=1'
-echo $def
+file_list=$(ls ${tmp_folder})
+for file in $file_list; do
+	file_path=${tmp_folder}/${file}
+
+	elapsed_time=$(cat $file_path | grep "Test elapsed time" | tail -n 1 | cut -d' ' -f8)
+	throughput=$(cat $file_path | grep "Test elapsed time" -A3 | tail -n 1)
+	echo "${file},${elapsed_time},${throughput}" >>${format_file}
 done
+
+sed -i -e "s/.out//g" ${format_file}
+sed -i -e "s/ENABLE_RECORDS_AUTO_CONVERT_TABLET_/,/g" ${format_file}
+sed -i -e "s/DEVICE_NUM_PER_WRITE_/,/g" ${format_file}
+sed -i -e "s/VECTOR_/,/g" ${format_file}
+sed -i -e "s/DEVICE_NUMBER_/,/g" ${format_file}
+sed -i -e "s/SENSOR_NUMBER_/,/g" ${format_file}
+sed -i -e "s/BATCH_SIZE_PER_WRITE_/,/g" ${format_file}
+sed -i -e "s/\-//g" ${format_file}
+sed -i -e "s/ \+/,/g" -e "s/,/,/g" ${format_file}
